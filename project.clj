97b31aa4db5c847ae9dc5e5d6d533563a2772ce8
Deon Moolman <cmdrdats@gmail.com>
2012-04-26 12:36:24 +0200

    WEB-1238 Spatula article search integration

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A simple rabbitmq abstraction"
   
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.cmdrdats/clj-plaza-rabbit "1.1.2-SNAPSHOT"]
+                 [com.rabbitmq/amqp-client "2.8.1"]
                  [cheshire "2.0.4"]
                  ])
